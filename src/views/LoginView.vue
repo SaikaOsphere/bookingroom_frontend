3db52e74ce58f45926c82dbe586acbcf7d6e1315
Sarawut_ulknows <sarawut12331@gmail.com>
2022-04-11 17:24:05 +0700

    Add card login.

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,10 +1,15 @@
 <template>
-  <div></div>
+  <div class="login">
+    <div class="d-flex align-content-end flex-wrap">
+      <b-card
+        img-src="https://www.beartai.com/wp-content/uploads/2021/08/23.png"
+        img-alt="Image"
+        img-top
+        style="max-width: 20rem"
+        class="mb-2 align-content-end"
+      >
+        Hello world
+      </b-card>
+    </div>
+  </div>
 </template>
-
-<script>
-export default {}
-</script>
-
-<style>
-</style>
