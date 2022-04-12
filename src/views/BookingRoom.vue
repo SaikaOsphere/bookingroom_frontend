40ac4016944b5ded9cb31a854fa80cf58b0a50c7
Sarawut_ulknows <sarawut12331@gmail.com>
2022-04-12 19:03:03 +0700

    Update booking ui.

--- src/views/BookingRoom.vue
+++ src/views/BookingRoom.vue
@@ -1,9 +1,127 @@
 <template>
   <div class="booking">
-    <b-col>Booking Room</b-col>
+    <!-- <b-container> -->
+    <b-row>
+      <b-col>
+        <b-row>
+          <b-col cols="4"> ตึก </b-col>
+          <b-col>
+            <b-form-group id="input-group-3" label-for="input-3">
+              <b-form-select
+                id="input-3"
+                v-model="test"
+                :options="foods"
+                required
+              ></b-form-select>
+            </b-form-group> </b-col
+        ></b-row>
+        <b-row
+          ><b-col cols="4">ชั้น</b-col
+          ><b-col>
+            <b-form-group id="input-group-3" label-for="input-3">
+              <b-form-select
+                id="input-3"
+                v-model="test"
+                :options="foods"
+                required
+              ></b-form-select>
+            </b-form-group> </b-col
+        ></b-row>
+      </b-col>
+      <b-col>
+        <b-row
+          ><b-col cols="4">ประเภท</b-col
+          ><b-col>
+            <b-form-group id="input-group-3" label-for="input-3">
+              <b-form-select
+                id="input-3"
+                v-model="test"
+                :options="foods"
+                required
+              ></b-form-select>
+            </b-form-group> </b-col
+        ></b-row>
+        <b-row
+          ><b-col cols="4">ขนาดห้อง</b-col
+          ><b-col>
+            <b-form-group id="input-group-3" label-for="input-3">
+              <b-form-select
+                id="input-3"
+                v-model="test"
+                :options="foods"
+                required
+              ></b-form-select>
+            </b-form-group> </b-col
+        ></b-row>
+      </b-col>
+      <b-col
+        ><b-col cols="6">รหัสห้อง</b-col
+        ><b-col>
+          <b-form-group id="input-group-3" label-for="input-3">
+            <b-form-select
+              id="input-3"
+              v-model="test"
+              :options="foods"
+              required
+            ></b-form-select>
+          </b-form-group> </b-col
+      ></b-col>
+      <b-col><b-button variant="primary" type="submit">ค้นหา</b-button></b-col>
+    </b-row>
+    <b-row align-v="stretch">
+      <b-table :items="items" :fields="fields" striped responsive="sm">
+        <template #cell(ดำเนินการ)>
+          <b-button size="sm" class="mr-2">เเก้ไข</b-button>
+          <b-button size="sm" class="mr-2">ลบ</b-button>
+        </template>
+      </b-table>
+    </b-row>
+    <!-- </b-container> -->
   </div>
 </template>
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      test: '',
+      foods: [
+        { text: 'Select One', value: null },
+        'Carrots',
+        'Beans',
+        'Tomatoes',
+        'Corn'
+      ],
+      fields: [
+        'ลำดับ',
+        'โค้ดห้อง',
+        'อุปกรณ์',
+        'ตึก',
+        'จำนวนคนที่รองรับ (คน)',
+        'ดำเนินการ'
+      ],
+      items: [
+        {
+          isActive: true,
+          ลำดับ: 40,
+          โค้ดห้อง: 'Dickerson',
+          อุปกรณ์: 'Macdonald',
+          ตึก: '',
+          จำนวนคนที่รองรับ: 5,
+          ดำเนินการ: ''
+        }
+      ]
+    }
+  }
+}
 </script>
-<style></style>
+<style>
+.screen {
+  padding-left: 10%;
+  padding-right: 10%;
+}
+
+b-col,
+b-row {
+  border: 2px solid black;
+}
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -7,50 +7,60 @@
       style="max-width: 20rem"
       class="mb-2 center"
     >
-      <b-card-text>
-        <div style="text-align: justify">
-          <b-form @submit="onSubmit" @reset="onReset" v-if="!loggedIn">
-            <b-form-group
-              id="input-group-1"
-              label="Username"
-              label-for="input-1"
-            >
-              <b-form-input
-                id="input-1"
-                v-model="form.username"
-                type="text"
-                placeholder="Username"
-                required
-              ></b-form-input>
-            </b-form-group>
+      <iframe
+        width="275"
+        height="200"
+        src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      >
+        <b-card-text>
+          <div style="text-align: justify">
+            <b-form @submit="onSubmit" @reset="onReset" v-if="!loggedIn">
+              <b-form-group
+                id="input-group-1"
+                label="Username"
+                label-for="input-1"
+              >
+                <b-form-input
+                  id="input-1"
+                  v-model="form.username"
+                  type="text"
+                  placeholder="Username"
+                  required
+                ></b-form-input>
+              </b-form-group>
 
-            <b-form-group
-              id="input-group-2"
-              label="Password:"
-              label-for="input-2"
-            >
-              <b-form-input
-                id="input-2"
-                v-model="form.password"
-                placeholder="Password"
-                required
-                type="password"
-              ></b-form-input>
-            </b-form-group>
+              <b-form-group
+                id="input-group-2"
+                label="Password:"
+                label-for="input-2"
+              >
+                <b-form-input
+                  id="input-2"
+                  v-model="form.password"
+                  placeholder="Password"
+                  required
+                  type="password"
+                ></b-form-input>
+              </b-form-group>
 
-            <b-button type="reset" variant="danger">Reset</b-button>
-            <b-button type="submit" variant="primary" class="m-2"
-              >Login</b-button
-            >
-          </b-form>
-          <b-form v-else @submit="logout">
-            You are logged in
-            <b-button type="submit" variant="primary" class="m-2">
-              Logout
-            </b-button>
-          </b-form>
-        </div>
-      </b-card-text>
+              <b-button type="reset" variant="danger">Reset</b-button>
+              <b-button type="submit" variant="primary" class="m-2"
+                >Login</b-button
+              >
+            </b-form>
+            <b-form v-else @submit="logout">
+              You are logged in
+              <b-button type="submit" variant="primary" class="m-2">
+                Logout
+              </b-button>
+            </b-form>
+          </div>
+        </b-card-text>
+      </iframe>
     </b-card>
     <!-- <b-card no-body class="overflow-hidden center" style="max-width: 540px;">
     <b-row no-gutters>
@@ -105,7 +115,7 @@ export default {
 
 <style scoped>
 .center {
-  position: absolute;
+  position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
